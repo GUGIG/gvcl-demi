6ef6290b9b688f37ad972cfb55f5113ec5d2f1b5
정재훈 <jhc060109@naver.com>
2022-07-12 17:21:23 +0900

    use library mode of vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { fileURLToPath, URL } from "url";
-
+import path from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import typescript from "@rollup/plugin-typescript";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,6 +11,25 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    outDir: "./lib",
+    lib: {
+      entry: path.resolve(__dirname, "src/components/index.ts"),
+      name: "gvcl",
+      fileName: (format) => `gvcl.${format}.js`,
+    },
+    rollupOptions: {
+      plugins: [
+        peerDepsExternal(),
+        typescript({
+          tsconfig: "./tsconfig.json",
+          include: ["src/components/**/*"],
+          declaration: true,
+          declarationDir: "lib/types",
+        }),
+      ],
     },
   },
 });
